9f9eddc850656c05f7d4f22dfb01974de3e84d30
Ding Xiao Mei <1466741044@qq.com>
2018-01-18 15:02:58 +0800

    creat react-helloworld

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # react_helloworld
 开启学习react之路
+1.创建react新项目
+下载安装node.js
+打开 node.js command prompt 切换目录到 d: cd d:\dxm03\react
+npm install -g create-react-app
+create-react-app react_helloworld
---
+++ react_helloworld/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+// import registerServiceWorker from './registerServiceWorker';
+console.log(parseFloat("10.2abc"));
+ReactDOM.render(<h1> Hello, world!</h1>, document.getElementById('root'));
+// registerServiceWorker();
